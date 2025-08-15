4964b6d5119226e63db3b9377730e129028846fd
Sittipon K <sittipon6324@gmail.com>
2025-08-15 22:21:20 +0700

    fix: update Android build configuration syntax

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.jenosize_test"
-        minSdk = 23              
-        targetSdk = 35         
+        minSdk = 23
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
